5ab242127145b377b1c30362e98dbcf9340a8beb
nuguni <nuguni@kakao.com>
2020-01-18 15:28:12 +0900

    add speech to text

--- src/run.py
+++ src/run.py
@@ -1,22 +1,27 @@
-import voice
-from vision import recognizer, cascade
-import utils
 
-RECOGNIZER_YML_PATH = "temp/train.yml"
+
 def main():
+    # 환자 시간 갖고 오기 
+    # 시간 확인 
+    # 약먹을 환자가 있으면 
+    # 주행 시작 
+    # 얼굴 확인 
+    # 환자인지 체크 음성 & 비전 
+    # 약 제조(?) 
+    # 약 내리기 
+    # 다음 환자 or 제자리 이동 
+    """
     while True:
-        # 주행
-        gray_face = cascade.find_gray_face() 
-        patient_id, confidence = recognizer.get_id(gray_face, RECOGNIZER_YML_PATH) 
-        patient_name, drug = utils.get_patient_info(patient_id) 
-        if patient_name != "":
-            message = "hello... {} 약 제조 중 입니다.".format(patient_name)
-            print (message)
-            #voice.tts(message)
-            # 약 제조 중
-            # open door
-                    
-        
+        patient = get_patient_info()
+        if patient == None:
+            go_home()
+            continue
+        drive()
+        face = face_detection()
+        if check_patinet(patient):
+            give_drug()
+    """
+    return 
 
 
 if __name__ == "__main__":
--- src/voice/stt.py
+++ src/voice/stt.py
@@ -1,4 +1,41 @@
+import sys
+import requests
+from  aiy.cloudspeech import CloudSpeechClient
+from aiy.board import Board
 
 # clova stt
+def clova_stt(lang="Kor", client_id, client_secret, voice_file_path):
+    #
+    # API : https://apidocs.ncloud.com/ko/ai-naver/clova_speech_recognition/stt/
+    # price : 4won/15sec 
+    #
+    url = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=" + lang
+    data = open(voice_file_path, 'rb')
+    headers = {
+        "X-NCP-APIGW-API-KEY-ID": client_id,
+        "X-NCP-APIGW-API-KEY": client_secret,
+        "Content-Type": "application/octet-stream"
+    }
+    response = requests.post(url,  data=data, headers=headers)
+    rescode = response.status_code
+    if(rescode == 200):
+        return response.text
+    else:
+        return "Error : {}".format(response.text)
 
 # google stt
+def google_stt(language_code = 'en-US' , hint_phrases=('yes', 'no')):
+    #
+    # make sure that assistant.json file is in the correct folder
+    # https://aiyprojects.readthedocs.io/en/latest/aiy.cloudspeech.html
+    # The Google Cloud Speech-to-Text service is a cloud-based service. If you use it for less than 60 minutes a month, it’s free. Beyond that, the cost is $0.006 for every 15 seconds. Don’t worry: you’ll get a reminder if you go over your free limit.
+    #
+    client = CloudSpeechClient()
+    with Board() as board:
+        while True:
+            text = client.recognize(language_code=language_code, hint_phrases=hint_phrases)
+            if text is None:
+                return ""
+            else:
+                return text
+    
